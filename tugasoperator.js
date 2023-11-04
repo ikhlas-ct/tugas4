f05651cc448fa12bad96afe85fd988d2745a6232
ikhlas-ct <ikhlasulamalmawardi@gmail.com>
2023-11-04 12:03:02 +0700

    proyek berhasil

---
+++ tugasoperator.js
@@ -0,0 +1,11 @@
+// Perintah :
+// Buatlah sebuah logika perhitungan dengan menggunakan operator aritmatika
+// dengan hasil penjumlahan sama dengan 15 dan hasil pengurangan dengan sama dengan 1
+// yang terlebih dahulu dibuatkan dalam varible let dulu, selamat mengerjakan !! 😎
+
+let a=8;
+let b=7;
+
+console.log ("Penjumlahan=",a + b)
+console.log ("Pnegurangan=",a - b)
+
---
+++ tugaspenugasan.js
@@ -0,0 +1,14 @@
+// Perintah :
+// let nama = "Senja"
+// let nilai = 80
+
+// maka logika dengan operator penugasan dengan menggunakan (pengisian dan penjumlahan),
+// semangat ya mengerjakannya !! 🤑
+
+let nama = "Senja"
+let nama1 = 20
+let nilai = 80
+console.log("Pengisian =",nama += nilai)
+console.log("Pengisian =",nama1 += nilai)
+
+
